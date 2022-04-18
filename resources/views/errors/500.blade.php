ee683fa66bb1a4683454b24dd90a64e633a48f37
gnamou <gnanmoussa@gmail.com>
2022-04-18 13:52:44 +0000

    Erreur 500

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="text-center text-secondary">
+        <strong>IErreur lors de la connexion a facebook</strong>
+        <div><a href="{{route('home')}}" class="btn btn-outline-secondary">Revenir en arrière</a></div>
+    </div>
+@endsection
