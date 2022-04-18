0f612db16d8d0f8d15d98c24b02535004df6ddaa
gnamou <gnanmoussa@gmail.com>
2022-04-18 19:20:59 +0000

    commit v

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="text-center text-secondary">
-        <strong>Erreur: vous n'avez aucune page facebook</strong>
+        <strong>Erreur de connexion a la page facebook</strong>
         <div><a href="{{route('home')}}" class="btn btn-outline-secondary">Revenir en arrière</a></div>
     </div>
 @endsection
